e57578e7049efd5db7485b058c65cc90740d59a9
Fraxoo <fraxo.officiel@gmail.com>
2025-10-24 11:34:23 +0200

    ajout color

--- Pokedex/src/components/pokemon-card.tsx
+++ Pokedex/src/components/pokemon-card.tsx
@@ -1,4 +1,9 @@
 import { PokemonItem } from "../models/Pokemon"
+import ColorThief from "colorthief";
+import { useEffect, useRef, useState } from "react";
+
+
+
 type PokemonCardProps = {
     pokemon: PokemonItem;
 };
@@ -6,10 +11,24 @@ type PokemonCardProps = {
 
 
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
+    const imgRef = useRef<HTMLImageElement | null>(null);
+    const [bg, setBg] = useState("");
+
+    useEffect(() => {
+        const img = imgRef.current;
+        if (!img) return;
+
+        img.onload = () => {
+            const colorthief = new ColorThief();
+            const [r, g, b] = colorthief.getColor(img);
+            setBg(`rgb(${r},${g},${b})`);
+        };
+    }, [pokemon.image])
+
     return (
-        <div className="pokemon-card">
+        <div className="pokemon-card" style={{ backgroundColor: bg, padding: "10px", borderRadius: 8 }}>
+            <img alt={pokemon.name} ref={imgRef} src={pokemon.image} crossOrigin="anonymous" />
             <h3>{pokemon.name}</h3>
-            <img src={pokemon.image} alt={pokemon.name}></img>
         </div>
-    )
+    );
 }
